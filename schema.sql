93b2bb220a427219f00303f03c509504d04ccc93
Will McAuliff <womcauliff@gmail.com>
2016-09-16 22:15:35 -0400

    Added mock data for db. Products table altered to handle mock data.

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ CREATE SCHEMA `Famazon` ;
 
 CREATE TABLE `Famazon`.`Products` (
   `ItemID` INT NOT NULL AUTO_INCREMENT,
-  `ProductName` VARCHAR(45) NOT NULL,
-  `DepartmentName` VARCHAR(45) NOT NULL,
+  `ProductName` VARCHAR(100) NOT NULL,
+  `DepartmentName` VARCHAR(50) NOT NULL,
   `Price` DECIMAL(10,2) NOT NULL,
   `StockQuantity` INT NOT NULL,
   PRIMARY KEY (`ItemID`));
---
+++ seeds.sql
@@ -0,0 +1,68 @@
+INSERT INTO `Famazon`.`Products`
+(
+	`ProductName`,
+	`DepartmentName`,
+	`Price`,
+	`StockQuantity`
+)
+VALUES
+(
+	"Xbox One 500GB Console - Name Your Game Bundle",
+	"Electronics",
+	249.99,
+	15
+),
+(
+	"Sony PlayStation 4 500GB Bundle with Call of Duty Black Ops III - Black",
+	"Electronics",
+	299.00,
+	20
+),
+(
+	'Tommy Hilfiger Elise Jacquard Crossbody',
+	'Clothing, Shoes & Jewelry', 
+	'78.00', 
+	'13'
+),
+(
+	'Calvin Klein Men\'s 38 mm Reversible Flat-Strap Leather Belt', 
+	'Clothing, Shoes & Jewelry', 
+	'26.99', 
+	'10'
+),
+(
+	'Acer Chromebook 15 CB5-571-C4T3 (15.6-Inch HD, 2GB RAM, 16GB SSD)',
+	'Electronics',
+	'213.00',
+	'20'
+),
+(
+	'Odyssey Men\'s White Hot SuperStroke RX Vline Fang Putter',
+	'Sports & Outdoors',
+	'154.39',
+	'7'
+),
+(
+	'JOOLA Inside Table Tennis Table with Net Set',
+	'Sports & Outdoors',
+	'399.98',
+	'5'
+),
+(
+	'Under Armour Men\'s CoolSwitch Golf Glove',
+	'Sports & Outdoors',
+	'16.99',
+	'25'
+),
+(
+	'Sennheiser RS120 On-Ear Wireless RF Headphones with Charging Dock',
+	'Movies, Music & Games',
+	'58.99',
+	'4'
+),
+(
+	'Bluedio T3 (Turbine 3rd) Extra Bass Wireless Bluetooth 4.1 Stereo Headphones(Black)',
+	'Movies, Music & Games',
+	'49.99',
+	'4'
+);
