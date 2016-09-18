56ced5c90bda6296dc0e28ab64b5daab5312546a
Will McAuliff <womcauliff@gmail.com>
2016-09-17 22:27:30 -0400

    Customer selects Product and quantity. Program calculates total.

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function setUpInventory(callback) {
 		//console.log(fields);
 		//parse row into Product object and add to inventory
 		rows.forEach(function(row) {
-			inventory[inventory.length] = new Product(
+			inventory[row.ItemID] = new Product(
 				row.ItemID, 
 				row.ProductName, 
 				row.DepartmentName,
@@ -48,27 +48,115 @@ function setUpInventory(callback) {
 			);
 		});
 
-		connection.end();
-
 		callback(null, inventory);
 	});	
 }
 
-function displayInventory(inventory) {
+function displayInventory(inventory, callback) {
 	console.log("displayInventory()");
+
 	inventory.forEach(function(Product) {
 		console.log(
 		"\n\t" + Product.getId() +
 		"\n\t" + Product.getProductName() +
 		"\n\t" + Product.getDepartmentName() +
 		"\n\t" + Product.getPrice()
-		)
+		);
 	});
+
+	callback(null);
 }
 
 function customer(inventory) {
 	console.log("customer()");
-	displayInventory(inventory);
+	displayInventory(inventory, shop);
+
+	function shop(err) {
+		console.log("shop()");
+		if(err) throw err;
+
+		inquirer.prompt([
+			{
+				name: 'productChoice',
+				type: 'list',
+				message: 'Which product would you like to buy?',
+				choices: function() {
+					var choices = [];
+					inventory.forEach(function(Product){
+						choices.push("[" + Product.getId().toString() + "] | " + Product.getProductName());
+					});
+					choices.push(new inquirer.Separator());
+					return choices;
+				}
+			},
+			{
+				name: 'quantity',
+				type: 'input',
+				message: 'How many of units of this product?',
+				default: function() {
+					return 1;
+				},
+				validate: function(value) {
+					if (isNaN(value) == false 
+						&& parseInt(value) > 0 
+						//&& parseInt(value) <= inventory[answers.productChoice].getStockQuantity()
+					){
+                    	return true;
+		            } 
+		            else {
+		                return false;
+		            }
+				}
+			}
+		]).then(function (answers) {
+			//console.log(answers.productChoice);
+			//console.log(answers.quantity);
+			var rx = /\d+/g;
+			var arr = rx.exec(answers.productChoice);
+			makeOrder(parseInt(arr[0]), answers.quantity, calculateTotal);
+		});
+	}
+	function makeOrder(pid, q, callback) {
+		console.log('makeOrder()');
+
+		if (q > inventory[pid].getStockQuantity()) {
+			console.log('Insufficient quantity!');
+			connection.end();
+			return;
+		}
+
+		console.log('pid' + pid);
+
+		connection.query(
+			'SELECT * FROM `Products` WHERE `ItemID` = ?',
+			[
+			    pid
+			],
+			function(err, rows, fields) {
+				if (err) throw err;
+
+				//console.log('The solution is: ', rows);
+				//console.log(fields);
+				//parse row into Product object and add to inventory
+				rows.forEach(function(row) {
+					console.log(row);
+				});
+
+				callback(null, pid, q);
+			}
+		);	
+	}
+	function calculateTotal(err, pid, q) {
+		console.log('calculateTotal()');
+		if (err) throw err;
+
+		console.log(
+			"Total transaction:"
+			+ "\n\t$" + parseFloat(Math.round((inventory[pid].getPrice() * q) * 100) / 100).toFixed(2) 
+		);
+
+		connection.end();
+	}
 }
 
 
